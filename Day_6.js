6d534d949f2b244d8bf4f92690aba437ca6b560a
Aditya <adityaadiraj74@gmail.com>
2024-07-18 21:05:00 +0530

    Update Day_6.js
    up to task 6

--- Day_6.js
+++ Day_6.js
@@ -2,9 +2,26 @@
 // Activity- 1 Array creation and access
 // Task 1- create an array of number 1 to 5 and log the array to the console.
 let arr1 = [1, 2, 3, 4, 5];
-console.log(arr1);
-console.log("----------------------------------------");
+// console.log(arr1);
 
-// Task 1- create an array of number 1 to 5 and log the array to the console.
+// Task 2- Access the first and last elements of an arrayand log them to the console.
 let arr2 = [1, 2, 3, 4, 5];
+// console.log(arr2[0]);
+// console.log(arr2.length);
+
+// Activity- 2 Array Methods
+// Task 3- use push method to add a new number to the end of array and log the updated array.
+let arr3 = arr1.push(6);
+// console.log(arr1);
+
+// Task 4- use pop method to remove the last element from the array and log the updated array.
+let arr4 = arr1.pop();
+// console.log(arr1);
+
+// Task 5- use sift method to remove the first element from the array and log the updated array.
+let arr5 = arr1.shift();
+// console.log(arr1);
+
+// Task 6- use unsift method to add element in the array and log the updated array.
+let arr6 = arr1.unshift(1);
 console.log(arr1);
