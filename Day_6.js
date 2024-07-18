d5bb298d4b6b14992a9c8c10038e962e0ac1d14f
Aditya <adityaadiraj74@gmail.com>
2024-07-18 19:05:58 +0530

    Create Day_6.js
    task-1

---
+++ Day_6.js
@@ -0,0 +1,10 @@
+//Day - 6
+// Activity- 1 Array creation and access
+// Task 1- create an array of number 1 to 5 and log the array to the console.
+let arr1 = [1, 2, 3, 4, 5];
+console.log(arr1);
+console.log("----------------------------------------");
+
+// Task 1- create an array of number 1 to 5 and log the array to the console.
+let arr2 = [1, 2, 3, 4, 5];
+console.log(arr1);
