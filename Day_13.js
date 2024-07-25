7b00ab962abd4224c083e5f3acdca60222b83a43
Aditya <adityaadiraj74@gmail.com>
2024-07-25 19:37:31 +0530

    Update Day_13.js
    complete

--- Day_13.js
+++ Day_13.js
@@ -109,3 +109,23 @@ async function customPromiseOne() {
 customPromiseOne();
 
 // Activity 5: Graceful Error handling in Fetch
+
+fetch("https://jsonplaceholder.api.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
+
+async function getAllUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("E:", error);
+  }
+}
+getAllUsers();
